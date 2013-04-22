e5c2449c993808879440373a97b83a9b866aa7b1
Stian Soiland-Reyes <stian@soiland-reyes.com>
2013-04-22 01:23:14 +0100

    CXF-4919 Re-enabling ignored tests

--- rt/frontend/jaxrs/src/test/java/org/apache/cxf/jaxrs/impl/LinkBuilderImplTest.java
+++ rt/frontend/jaxrs/src/test/java/org/apache/cxf/jaxrs/impl/LinkBuilderImplTest.java
@@ -18,18 +18,16 @@
  */
 package org.apache.cxf.jaxrs.impl;
 
+import static org.junit.Assert.assertEquals;
+
 import java.net.URI;
 
 import javax.ws.rs.core.Link;
 
-import org.junit.Ignore;
 import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
 
 public class LinkBuilderImplTest {
     
-
     @Test
     public void build() throws Exception {
         Link.Builder linkBuilder = new LinkBuilderImpl();
@@ -37,7 +35,6 @@ public class LinkBuilderImplTest {
         assertEquals("<http://example.com/page1>;rel=\"previous\"", prevLink.toString());
     }
 
-    @Ignore("Ignored due to CXF-4919")
     @Test
     public void relativeBuild() throws Exception {
         Link.Builder linkBuilder = new LinkBuilderImpl();
@@ -64,7 +61,6 @@ public class LinkBuilderImplTest {
         assertEquals("<http://example.com/page3>;rel=\"next\"", nextLink.toString());
     }
 
-    @Ignore("Ignored due to CXF-4919")
     @Test
     public void copyOnRelativeBuild() throws Exception {
         Link.Builder linkBuilder = new LinkBuilderImpl();
